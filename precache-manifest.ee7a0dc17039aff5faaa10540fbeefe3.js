662e16b1ba995d9b77a1038f9243d877aa600d49
kou <1649369473@qq.com>
2020-02-07 10:52:42 +0800

    commit

---
+++ precache-manifest.ee7a0dc17039aff5faaa10540fbeefe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1d3cbafb7fc870b0228e293af4118d",
+    "url": "/water-tap/index.html"
+  },
+  {
+    "revision": "2025bd24a948c29c9b49",
+    "url": "/water-tap/static/css/main.22a71e13.chunk.css"
+  },
+  {
+    "revision": "a7eb8f9cdef966922114",
+    "url": "/water-tap/static/js/2.e8ce7aa1.chunk.js"
+  },
+  {
+    "revision": "99c5655d2432b8199a89e031b7b8d3f7",
+    "url": "/water-tap/static/js/2.e8ce7aa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2025bd24a948c29c9b49",
+    "url": "/water-tap/static/js/main.cfb5c0d7.chunk.js"
+  },
+  {
+    "revision": "7172ed5699e7696ce4c6",
+    "url": "/water-tap/static/js/runtime-main.b8ea2553.js"
+  }
+]);
